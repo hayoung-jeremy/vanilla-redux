c01e28cbc81f8839d778993c9172079766c3a9ab
Ha young, Kim <jeremiah91@naver.com>
2021-04-27 17:39:30 +0900

    use SWITCH instead of IF

--- src/index.js
+++ src/index.js
@@ -5,20 +5,24 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("number");
 
+// action types :
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // 2) create reducer : function that modifies the data in the store (only this function can)
 // state = 0 : initial state value 설정
 // action : data를 변형시키는 함수
 const countModifier = (count = 0, action) => {
   // 여기서 count 는 current count
-  console.log(count, action);
+  console.log("current count : " + count, action);
   // 전달받은 action이 무엇인지에 따라 데이터를 어떻게 변경할 지 작성
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    // return 하는것이 곧 data가 됨
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -41,8 +45,8 @@ countStore.subscribe(onChange);
 // console.log(countStore.getState) // --> state : reducer 가 return 하는 값, 즉 변화시킨 data 값을 가져옴
 
 add.addEventListener("click", () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 });
 minus.addEventListener("click", () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 });
