74bd7f2f8e27a29c6b3f3783329182b8174f8742
Ha young, Kim <jeremiah91@naver.com>
2021-04-27 17:26:11 +0900

    use redux

--- src/index.js
+++ src/index.js
@@ -7,13 +7,41 @@ const number = document.getElementById("number");
 
 // 2) create reducer : function that modifies the data in the store (only this function can)
 // state = 0 : initial state value 설정
-const countModifier = (state = 0) => {
-  // return 하는것이 곧 data가 됨
-  console.log(state);
-  return state;
+// action : data를 변형시키는 함수
+const countModifier = (count = 0, action) => {
+  // 여기서 count 는 current count
+  console.log(count, action);
+  // 전달받은 action이 무엇인지에 따라 데이터를 어떻게 변경할 지 작성
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    // return 하는것이 곧 data가 됨
+    return count;
+  }
 };
 
 // 1) create store
 const countStore = createStore(countModifier);
 
+const onChange = () => {
+  console.log(countStore.getState());
+  number.innerText = countStore.getState();
+};
+
+// 4) subscribe : store의 data 변화를 알아낼수 있게 해준다
+countStore.subscribe(onChange);
+
+// 3) dipatch() 를 사용하여 action을 만들 수 있음 () 는 항상 object여야함
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "MINUS" });
+
 // console.log(countStore.getState) // --> state : reducer 가 return 하는 값, 즉 변화시킨 data 값을 가져옴
+
+add.addEventListener("click", () => {
+  countStore.dispatch({ type: "ADD" });
+});
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: "MINUS" });
+});
