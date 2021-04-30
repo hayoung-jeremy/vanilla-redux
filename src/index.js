93dad860da9cf295ac840a6a877ef31ecc4332d6
Ha young, Kim <jeremiah91@naver.com>
2021-04-30 09:19:04 +0900

    giving id to toDo

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
 const reducer = (state = [], action) => {
-  console.log(action);
+  // console.log(action);
   switch (action.type) {
     case ADD_TODO:
       // return state.push(action.text) : redux에서 이렇게 직접 데이터를 변경할 수는 없다
       // old[]의 contents 를 가져와서 new contents와 함께 new array 에 넣는다.
       // 만약 빈 input.value 를 가져왔을 경우엔 원래 state 를, 내용이 입력되었을 경우에만 업데이트된 state을 반환한다.
-      return action.text !== "" ? [...state, { text: action.text }] : state;
+      return action.text !== ""
+        ? [...state, { text: action.text, id: action.liId }]
+        : state;
     case DELETE_TODO:
       return [];
     default:
@@ -26,15 +28,17 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
-const addToDo = (text) => {
-  store.dispatch({ type: ADD_TODO, text });
+const addToDo = (text, liId) => {
+  store.dispatch({ type: ADD_TODO, text, liId });
 };
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addToDo(toDo);
+  // 제출 시 해당 to do list에 새 아이디 부여
+  const liId = Date.now();
+  addToDo(toDo, liId);
 };
 
 form.addEventListener("submit", onSubmit);
