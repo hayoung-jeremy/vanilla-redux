df6ace7eb210b8179697cfa6174b1c482566f96a
Ha young, Kim <jeremiah91@naver.com>
2021-05-06 00:04:40 +0900

    create actions

--- src/store.js
+++ src/store.js
@@ -1,29 +1,17 @@
 import { createStore } from "redux";
-
-const ADD = "ADD";
-const DELETE = "DELETE";
+import { createAction } from "@reduxjs/toolkit";
 
 // actions :
-const addToDo = (text, id) => {
-  return {
-    type: ADD,
-    text,
-    id: Date.now(),
-  };
-};
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [...state, { text: action.text, id: action.id }];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      console.log(action);
+      return [...state, { text: action.payload, id: action.id }];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
