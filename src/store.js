fef5d678c97763b6c9989be2fd46c99339d84eea
Ha young, Kim <jeremiah91@naver.com>
2021-05-06 10:41:26 +0900

    creating reducer w/ redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,21 +1,34 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // actions :
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      console.log(action);
-      return [...state, { text: action.payload, id: action.id }];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       console.log(action);
+//       return [...state, { text: action.payload, id: action.id }];
+//     case deleteToDo.type:
+//       console.log(action);
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+// redux toolkit : state 을 mutate 하는것이 가능해짐
+// 이는 immer 때문
+const reducer = createReducer([], {
+  // 또한, 아래처럼 state 을 직접 mutate 할 수도 있고, {}
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  // 아니면, 이전처럼 새로운 state 을 return 할 수도 있다. w/o {}
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const store = createStore(reducer);
 
