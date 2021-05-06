cc6cc70b8faa1405e918a7e342b665047e2c870a
Ha young, Kim <jeremiah91@naver.com>
2021-05-06 16:13:35 +0900

    createSlice

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { actionCreators } from "../store";
+import { add } from "../store";
 import ToDo from "../components/ToDo";
 
 function Home({ toDos, addToDo }) {
@@ -44,7 +44,7 @@ function mapStateToProps(state) {
 // mapDispatchToProps (store.dispatch, ownProps from Component)
 function mapDispatchToProps(dispatch) {
   return {
-    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+    addToDo: (text) => dispatch(add(text)),
   };
 }
 
--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
-import { createStore } from "redux";
-import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+import {
+  // createAction,
+  // createReducer,
+  configureStore,
+  createSlice,
+} from "@reduxjs/toolkit";
 
 // actions :
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
 
 // const reducer = (state = [], action) => {
 //   switch (action.type) {
@@ -20,22 +25,29 @@ const deleteToDo = createAction("DELETE");
 
 // redux toolkit : state 을 mutate 하는것이 가능해짐
 // 이는 immer 때문
-const reducer = createReducer([], {
-  // 또한, 아래처럼 state 을 직접 mutate 할 수도 있고, {}
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
+// const reducer = createReducer([], {
+//   // 또한, 아래처럼 state 을 직접 mutate 할 수도 있고, {}
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() });
+//   },
+//   // 아니면, 이전처럼 새로운 state 을 return 할 수도 있다. w/o {}
+//   [deleteToDo]: (state, action) =>
+//     state.filter((toDo) => toDo.id !== action.payload),
+// });
+
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  // 아니면, 이전처럼 새로운 state 을 return 할 수도 있다. w/o {}
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
+export const { add, remove } = toDos.actions;
+
 // redux dev tools 를 사용하여 state, action, dispatch 등 여러가지를 확인할 수 있음
-const store = configureStore({ reducer });
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
